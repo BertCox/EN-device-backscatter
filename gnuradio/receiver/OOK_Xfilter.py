70046056ebc959c6bc29be14c9a2ce032697e783
BertCox <31063583+BertCox@users.noreply.github.com>
2023-12-15 16:08:01 +0100

    OOK with tags
    Updated the OOK demodulator with tagged streams (decoding)

--- gnuradio/receiver/OOK_Xfilter.py
+++ gnuradio/receiver/OOK_Xfilter.py
@@ -26,6 +26,7 @@ from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
 from gnuradio import blocks
+from gnuradio import digital
 from gnuradio import filter
 from gnuradio import gr
 import sys
@@ -79,7 +80,7 @@ class OOK_Xfilter(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate = 2000000
         self.cutoff_freq = cutoff_freq = 50e3
         self.bandpass_width = bandpass_width = 100e3
-        self.numbers = numbers = 200
+        self.numbers = numbers = 2000
         self.lowpass_filter = lowpass_filter = firdes.low_pass(1, samp_rate, cutoff_freq, transition_width, firdes.WIN_HAMMING, 6.76)
         self.freq_offset = freq_offset = 520000
         self.freq = freq = 910E6
@@ -89,7 +90,7 @@ class OOK_Xfilter(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self._numbers_range = Range(1, 2000, 10, 200, 200)
+        self._numbers_range = Range(1, 2000, 10, 2000, 200)
         self._numbers_win = RangeWidget(self._numbers_range, self.set_numbers, 'numbers', "counter_slider", float)
         self.top_grid_layout.addWidget(self._numbers_win)
         self.uhd_usrp_source_0 = uhd.usrp_source(
@@ -106,10 +107,57 @@ class OOK_Xfilter(gr.top_block, Qt.QWidget):
         self.uhd_usrp_source_0.set_antenna("RX2", 0)
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
         # No synchronization enforced.
+        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
+            1024, #size
+            samp_rate/numbers, #samp_rate
+            "Na", #name
+            1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0_0.enable_tags(True)
+        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0_0.enable_grid(False)
+        self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            4096, #size
-            samp_rate, #samp_rate
-            "", #name
+            1024, #size
+            samp_rate/numbers, #samp_rate
+            "Voor", #name
             1 #number of inputs
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
@@ -154,8 +202,11 @@ class OOK_Xfilter(gr.top_block, Qt.QWidget):
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.freq_xlating_fir_filter_lp = filter.freq_xlating_fir_filter_ccc(decimation, lowpass_filter, freq_offset, samp_rate)
+        self.digital_correlate_access_code_tag_xx_0 = digital.correlate_access_code_tag_bb('1010101011', 0, 'preamble')
+        self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
         self.blocks_threshold_ff_0 = blocks.threshold_ff(1E-6, 3E-6, 0)
         self.blocks_keep_one_in_n_0 = blocks.keep_one_in_n(gr.sizeof_float*1, numbers)
+        self.blocks_float_to_char_1 = blocks.float_to_char(1, 1)
         self.blocks_complex_to_mag_squared_1 = blocks.complex_to_mag_squared(1)
 
 
@@ -164,8 +215,12 @@ class OOK_Xfilter(gr.top_block, Qt.QWidget):
         # Connections
         ##################################################
         self.connect((self.blocks_complex_to_mag_squared_1, 0), (self.blocks_threshold_ff_0, 0))
+        self.connect((self.blocks_float_to_char_1, 0), (self.digital_correlate_access_code_tag_xx_0, 0))
+        self.connect((self.blocks_keep_one_in_n_0, 0), (self.blocks_float_to_char_1, 0))
         self.connect((self.blocks_keep_one_in_n_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.blocks_threshold_ff_0, 0), (self.blocks_keep_one_in_n_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.qtgui_time_sink_x_0_0, 0))
+        self.connect((self.digital_correlate_access_code_tag_xx_0, 0), (self.blocks_uchar_to_float_0, 0))
         self.connect((self.freq_xlating_fir_filter_lp, 0), (self.blocks_complex_to_mag_squared_1, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.freq_xlating_fir_filter_lp, 0))
 
@@ -189,7 +244,8 @@ class OOK_Xfilter(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.set_bandpass_filter(firdes.band_pass(1.0, self.samp_rate, 1, self.bandpass_width/2, self.bandpass_width))
         self.set_lowpass_filter(firdes.low_pass(1, self.samp_rate, self.cutoff_freq, self.transition_width, firdes.WIN_HAMMING, 6.76))
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.numbers)
+        self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate/self.numbers)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
     def get_cutoff_freq(self):
@@ -212,6 +268,8 @@ class OOK_Xfilter(gr.top_block, Qt.QWidget):
     def set_numbers(self, numbers):
         self.numbers = numbers
         self.blocks_keep_one_in_n_0.set_n(self.numbers)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.numbers)
+        self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate/self.numbers)
 
     def get_lowpass_filter(self):
         return self.lowpass_filter
